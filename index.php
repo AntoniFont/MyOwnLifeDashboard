a012bd07a4e5ecf5591570452574123cad4dd583
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2022-12-05 00:02:45 +0000

    Started the data entry UI

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'header.php'; ?>
+
+<div class ="row">
+    <div class ="col"> 
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+            Selecciona asignatura
+        </button>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">ADIIU</a></li>
+            <li><a class="dropdown-item" href="#">Xarxes</a></li>
+            <li><a class="dropdown-item" href="#">Solucions</a></li>
+            <li><a class="dropdown-item" href="#">Qualitat</a></li>
+            <li><a class="dropdown-item" href="#">GP</a></li>
+            <li><a class="dropdown-item" href="#">Concurrent</a></li>
+        </ul>
+    </div>    
+</div>
+
+<?php include 'footer.php'; ?>
