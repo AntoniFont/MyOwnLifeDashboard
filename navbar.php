b951db64a7b2686c859eeffd22ec8e3db5dffd2a
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2022-12-12 15:20:35 +0000

    Added a navbar

---
+++ navbar.php
@@ -0,0 +1,21 @@
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Example 1 <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Example 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Example 3</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
