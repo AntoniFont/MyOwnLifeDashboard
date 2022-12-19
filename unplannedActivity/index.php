545ddd612c01101e97c423e827ed1bb4c668cfb4
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2022-12-19 18:19:49 +0000

    Added the beta to some new features

--- unplannedActivity/index.php
+++ unplannedActivity/index.php
@@ -74,27 +74,70 @@
             </div>
 
         </div>
-        <div class="row">
+
+
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex justify-content-center">
+                        <button type="button" class="btn btn-primary" id="timerButton">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-stopwatch" viewBox="0 0 16 16">
+                                <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"></path>
+                                <path
+                                    d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z">
+                                </path>
+                            </svg>
+                            <p>Start Timer</p>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+
+            <br><br><br><br><br><br>
+
+            <div class="row">
+                <div class="col">
+                    <h2>FEATURES THAT WORK, BUT SHOULD BE MADE PRETTIER: </h2>
+                </div>
+            </div>
+
+
+            <div class="row">
             <div class="col">
-                <div class="d-flex justify-content-center">
-                    <button type="button" class="btn btn-primary" id="timerButton">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-stopwatch" viewBox="0 0 16 16">
-                            <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"></path>
-                            <path
-                                d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z">
-                            </path>
-                        </svg>
-                        <p>Start Timer</p>
-                    </button>
+                
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                    Estaba realizando una tarea de grupo en grupo.
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                    <label class="form-check-label" for="flexRadioDefault2">
+                    No estaba realizando una tarea de grupo en grupo.
+                    </label>
+                </div>
+
+                <br>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault3">
+                    <label class="form-check-label" for="flexRadioDefault3">
+                        Estaba trabajando sin compañia.
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault2" id="flexRadioDefault4">
+                    <label class="form-check-label" for="flexRadioDefault4">
+                        Estaba trabajando con compañía de amigos (online o presencial).
+                    </label>
                 </div>
 
             </div>
+
+
         </div>
-
-    
-
-    </div>
 </body>
 
 </html>
