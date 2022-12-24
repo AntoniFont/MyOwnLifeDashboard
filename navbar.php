eb71c853a175d2b785d7cdb0175a3d9c6f2fa098
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2022-12-24 18:15:50 +0100

    new main Page

--- navbar.php
+++ navbar.php
@@ -1,30 +1,6 @@
 <div class="container-fluid">
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark " >
 <div class="container-fluid">
-<a class="navbar-brand" href="">Dashboard academico y personal</a>
-  <button class="navbar-toggler bg-dark" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" >
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="/myownlifedashboard/mainPage/index.php" id="mainpage">Main page</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/myownlifedashboard/unplannedActivity" id="unplannedActivityNavbar">Unplanned Activity</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Example 3</a>
-      </li>
-    </ul>
-  </div>
-  </div>
+<a class="navbar-brand" href="">Academic & personal dashboard</a>  
 </nav>
 </div>
-
-<script>
-    let params = new URLSearchParams(document.location.search);
-    let name = params.get("name"); 
-    $("#mainpage").attr("href",$("#mainpage").attr("href") + "?name=" + name)
-    $("#unplannedActivityNavbar").attr("href",$("#unplannedActivityNavbar").attr("href") + "?name=" + name)
-</script>
