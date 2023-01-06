32aaf050334404ce183bbe4e71dd86a6142816a2
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-01-06 21:49:40 +0100

    Now chart1And2 display the name of the course even if the total time is 0

--- academic/mainPage/backend/chart1And2getData.php
+++ academic/mainPage/backend/chart1And2getData.php
@@ -4,7 +4,17 @@ require dirname(__DIR__, 3)."/connectToTheDatabase.php";
 //CONNECT TO THE DB
 $conection = connectToTheDatabase();
 ///GET THE DATA INTO VARIABLES 
-$query = "SELECT sum(duration)/3600 as \"duracion\",courses100.name FROM `studydata100` JOIN courses100 ON studydata100.courseID = courses100.courseID JOIN user100 ON studydata100.userID = user100.id WHERE courses100.courseID is not NULL AND initialTime > (UNIX_TIMESTAMP() - (60 * 60 *24 * 7 *2)) AND nickname = \"".$_GET["name"]."\" GROUP BY studydata100.courseID";
+//get from the username, the id, for example the username "peter" may be the id 2
+$query = "select id from user100 where nickname=\"".$_GET["name"]."\"" ;
+$idCon = mysqli_query($conection, $query);
+$id = mysqli_fetch_all($idCon)[0][0];
+
+//SELECT COALESCE(sum(duration),0) as "duracion",name FROM courses100 LEFT JOIN studydata100 ON (courses100.courseID = studydata100.courseID) AND studydata100.initialTime>(UNIX_TIMESTAMP()-60*60*24*7*2) WHERE courses100.user=4 GROUP BY courses100.courseID; 
+//The total time of the selected student studied in each course in the last 2 weeks, for example:
+//  Maths - 1hours in the last 2 weeks
+//  PE - 0hours in the last 2 weeks
+//  Chemistry 1.5hours in the last 2 weeks
+$query = "SELECT COALESCE(sum(duration),0) as \"duracion\",courses100.name FROM courses100 LEFT JOIN studydata100 ON (courses100.courseID = studydata100.courseID) AND studydata100.initialTime>(UNIX_TIMESTAMP()-60*60*24*7*2) WHERE courses100.user=" . $id . " GROUP BY courses100.courseID";
 $coursesCon = mysqli_query($conection, $query);
 $resultado = mysqli_fetch_all($coursesCon);
 mysqli_close($conection);
