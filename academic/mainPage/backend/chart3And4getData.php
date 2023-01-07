da53bdffbfbe226a81c96e5c9124d1e7ff93dc42
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-01-07 15:17:27 +0100

    Now the query displays days ellapsed

--- academic/mainPage/backend/chart3And4getData.php
+++ academic/mainPage/backend/chart3And4getData.php
@@ -4,8 +4,17 @@ require dirname(__DIR__, 3)."/connectToTheDatabase.php";
 //CONNECT TO THE DB
 $conection = connectToTheDatabase();
 ///GET THE DATA INTO VARIABLES
-//SELECT sum(duration), FROM_UNIXTIME(initialTime, '%W, %d %M %Y') FROM `studydata100` JOIN user100 ON user100.id = studydata100.userID WHERE initialTime > (UNIX_TIMESTAMP() - (60 * 60 *24 * 7 *2)) AND nickname="toniet" GROUP BY FROM_UNIXTIME(initialTime, '%W, %d %M %Y') ORDER BY initialTime ASC
-$query = "SELECT sum(duration),FROM_UNIXTIME(initialTime,  \"%d-%m-%Y\") FROM studydata100 JOIN user100 ON user100.id = studydata100.userID WHERE initialTime > (UNIX_TIMESTAMP() - (60 * 60 *24 * 7 *2)) AND nickname=\"".$_GET["name"]."\" GROUP BY FROM_UNIXTIME(initialTime,  \"%d-%m-%Y\") ORDER BY initialTime ASC";
+/*
+SELECT sum(duration),
+FROM_UNIXTIME(initialTime, "%d-%m-%Y") as dia,
+FROM_UNIXTIME(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(STR_TO_DATE(FROM_UNIXTIME(initialTime,"%d-%m-%Y"),"%d-%m-%Y")),"%d")  as daysEllapsed 
+FROM studydata100 JOIN user100 ON user100.id = studydata100.userID WHERE initialTime > (UNIX_TIMESTAMP() - (60 * 60 *24 * 7 *2)) AND nickname="juanjo" GROUP BY FROM_UNIXTIME(initialTime, "%d-%m-%Y") ORDER BY initialTime ASC
+
+*/
+
+$query = "SELECT sum(duration),FROM_UNIXTIME(initialTime, \"%d-%m-%Y\") as dia,FROM_UNIXTIME(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(STR_TO_DATE(FROM_UNIXTIME(initialTime,\"%d-%m-%Y\"),\"%d-%m-%Y\")),\"%d\")  as daysEllapsed FROM studydata100 JOIN user100 ON user100.id = studydata100.userID WHERE initialTime > (UNIX_TIMESTAMP() - (60 * 60 *24 * 7 *2)) AND nickname=\"".$_GET["name"]."\" GROUP BY FROM_UNIXTIME(initialTime, \"%d-%m-%Y\") ORDER BY initialTime ASC";
+echo $query;
+echo "<br>";
 $dataCon = mysqli_query($conection, $query);
 $resultado = mysqli_fetch_all($dataCon);
 mysqli_close($conection);
