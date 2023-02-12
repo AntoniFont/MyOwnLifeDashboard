2c24b195edb2f2d32ee6cfafb690a039bc97bc10
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-02-12 21:24:16 +0100

    Only show active courses

--- academic/unplannedActivity/backend/getCoursesAndStudyTypes.php
+++ academic/unplannedActivity/backend/getCoursesAndStudyTypes.php
@@ -5,7 +5,7 @@ require dirname(__DIR__, 3)."/DatabaseManager.php";
 //CONNECT TO THE DB
 $dbManager = new DatabaseManager();
 ///GET THE DATA INTO VARIABLES 
-$sql = "select courseID, name from courses100 JOIN user100 ON courses100.user = user100.id where nickname=:nombre";
+$sql = "select courseID, name from courses100 JOIN user100 ON courses100.user = user100.id where nickname=:nombre AND UNIX_TIMESTAMP(STR_TO_DATE(courses100.finalDate, \"%Y-%m-%d\")) > UNIX_TIMESTAMP() ";
 $courses = $dbManager->query($sql, ["nombre" => $_GET["name"]]);
 $sql = "select typeStudyDataID ,name,description from typesstudydata100 ";
 $typesStudyData = $dbManager->query($sql, []);
