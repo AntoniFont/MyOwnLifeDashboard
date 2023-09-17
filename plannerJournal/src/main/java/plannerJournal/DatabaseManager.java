16032cbf54d24eb452e94ce23d9b5f89f6debe01
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-09-17 19:05:53 +0200

    Possible bugfix

--- plannerJournal/src/main/java/plannerJournal/DatabaseManager.java
+++ plannerJournal/src/main/java/plannerJournal/DatabaseManager.java
@@ -29,8 +29,7 @@ public class DatabaseManager {
     /* DATABASE CONSTANTS */
     //private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mysql://localhost:3306/";
-    private static final String DB_NAME = "myowndashboard";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/myowndashboard?connectTimeout=60000&socketTimeout=60000";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
@@ -44,7 +43,7 @@ public class DatabaseManager {
         // Open connection with database jdbc
         try {
             Class.forName(JDBC_DRIVER);
-            connection = DriverManager.getConnection(DB_URL + DB_NAME, USER, PASSWORD);     
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);     
             StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
             StackTraceElement caller = stackTraceElements[2]; 
             System.out.println("[debug] Database opened by " + caller.getClassName() + "." + caller.getMethodName() + "()");
