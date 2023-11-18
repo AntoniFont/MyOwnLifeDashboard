43e6b09d99f6392b75020149ade37d5ca79ea0d5
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-18 19:25:27 +0100

    bugfix

--- dashboard/view/login/loginBackend.php
+++ dashboard/view/login/loginBackend.php
@@ -1,18 +1,15 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/myownlifedashboard/dashboard/controller/DataAccessObjects/UserDAO.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/myownlifedashboard/dashboard/model/database/DatabaseManager.php");
 session_start();
 $usuariDAO = new UserDAO();
 $user = $usuariDAO->getUserFromNickname($_POST["username"]);
-
-$hashedInput = password_hash($_POST["key"],PASSWORD_DEFAULT);
-
-
-if($hashedInput == $user->getPasswordHash() ){
+if (password_verify($_POST["key"],$user->getPasswordHash())){
     $_SESSION["loggedIn"] = true;
     $_SESSION["username"] = $_POST["username"];
     echo "<p>Correcto! </p>";
     echo "<a href=\"../index.php?name=".$_POST["username"]."\"><p>Volver a la página principal</p></a>";
-} else {
+}else{
     echo "<p>Incorrecto! </p>";
     echo "<a href=\"./login.php\"><p>Volver a intentar</p></a>";
 }
