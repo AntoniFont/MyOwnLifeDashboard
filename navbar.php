2d96dd42bce48fec50caf86a41eb60b36bde0ad5
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-18 18:11:18 +0100

    unused file

--- navbar.php
+++
@@ -1,6 +0,0 @@
-<div class="container-fluid">
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark " >
-<div class="container-fluid">
-<a class="navbar-brand" href="">Academic & personal dashboard</a>  
-</nav>
-</div>
