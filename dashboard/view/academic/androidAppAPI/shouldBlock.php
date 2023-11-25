3369bf3c2e84492a8e3624e317922289f4cf1abd
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-25 18:06:57 +0100

    Update shouldBlock.php

--- dashboard/view/academic/androidAppAPI/shouldBlock.php
+++ dashboard/view/academic/androidAppAPI/shouldBlock.php
@@ -1,3 +1,14 @@
 <?php
-echo "yes";
+require_once($_SERVER["DOCUMENT_ROOT"] . "/myownlifedashboard/dashboard/controller/DataAccessObjects/StudyDataDAO.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/myownlifedashboard/dashboard/controller/DataAccessObjects/UserDAO.php");
+
+$StudyDataDAO = new StudyDataDAO();
+$user = (new UserDAO())->getUserFromNickname("toni");
+$today = date("Y-m-d");
+$seconds = $StudyDataDAO->getCriticalSecondsStudiedOfTheDay($user,$today,0);
+if($seconds>= 3600){
+    echo  "yes";
+}else{
+    echo "no";
+}
 ?>
