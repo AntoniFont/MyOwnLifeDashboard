6b665446d9055d0ac0522d08916bd6c91e5f2c6e
Toni Font <58040459+AntoniFont@users.noreply.github.com>
2023-11-28 16:51:37 +0100

    Create shouldBlockSufi.php

---
+++ dashboard/view/academic/androidAppAPI/shouldBlockSufi.php
@@ -0,0 +1,26 @@
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"] . "/myownlifedashboard/dashboard/controller/DataAccessObjects/StudyDataDAO.php");
+require_once($_SERVER["DOCUMENT_ROOT"] . "/myownlifedashboard/dashboard/controller/DataAccessObjects/UserDAO.php");
+
+$StudyDataDAO = new StudyDataDAO();
+$userDao = (new UserDAO());
+$user = $userDao->getUserFromNickname("sufi.mago");
+$currently = $userDao->getCurrentlyStudying("sufi.mago");
+$today = date("Y-m-d");
+$seconds = $StudyDataDAO->getCriticalSecondsStudiedOfTheDay($user,$today,14);
+if($currently == 1){
+    echo "no";
+}else{
+    $currentHour = date('G');
+    echo $currentHour;
+    if($currentHour < 16){
+        echo "no";
+    }else{
+        if($seconds>= 3600){
+            echo "no";
+        }else{
+            echo "yes";
+        }
+    }
+}
+?>
